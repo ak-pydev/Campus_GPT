5089cbdab3557ef41db46751240d3ea8aa2a01ce
ak-pydev <ak.iscoding@gmail.com>
2026-01-31 01:04:53 -0500

    Update theme to NKU colors (White/Black/Gold) and center layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="bg-black min-h-screen text-white">
+    <div className="bg-background text-foreground min-h-screen flex items-center justify-center">
       <AnimatedAIChat />
     </div>
   );

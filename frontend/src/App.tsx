a947ef584552264ba3413823e36d909c2682c9e2
ak-pydev <ak.iscoding@gmail.com>
2026-01-31 00:54:53 -0500

    Setup frontend with Vite, Tailwind, and AnimatedAIChat component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+import AnimatedAIChat from './components/mvpblocks/animated-ai-chat';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-black min-h-screen text-white">
+      <AnimatedAIChat />
+    </div>
+  );
+}
+
+export default App;
